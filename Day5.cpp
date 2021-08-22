a86792e7e77c75453f0423f9d46b47a2716b1535
Rahma3100 <88751172+Rahma3100@users.noreply.github.com>
2021-08-22 04:26:01 +0200

    Create Day5.cpp

---
+++ Day5.cpp
@@ -0,0 +1,31 @@
+# include <stdio.h>
+#include <stdlib.h>
+# include <string.h>
+	struct Course {
+		float credit_Hours ;
+		float student_score; 
+	};
+	struct student {
+		char courses_name [2][25];
+		float GPA;
+	};
+void calculate_GPA(float score1, float score2, float credit_hours1, float credit_hours2){
+	float GPA =((score1*credit_hours1)+(score2*credit_hours2)) / (credit_hours1 + credit_hours2);
+	printf ("Your GPA is: %f\n",GPA);
+}
+ 
+ struct Course X[2];
+ struct student Admin;
+int main(){
+
+	strcpy (Admin.courses_name[0],"Mathematics");
+	strcpy (Admin.courses_name[1],"Programming");
+	X[0].credit_Hours=3;
+	X[1].credit_Hours=4;
+	printf("Enter Mohamed's Score in Mathematics (Out of 4):  ");
+	scanf("%f",&X[0].student_score);
+	printf("Enter Mohamed's Score in Programming (Out of 4):  ");
+	scanf("%f",&X[1].student_score);
+	calculate_GPA(X[0].student_score, X[1].student_score, X[0].credit_Hours, X[1].credit_Hours );
+system ("pause");
+}
